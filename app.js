492e4ad579ce888bcf9b4049c7e794dcd18e701e
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-08-01 02:56:26 +0000

    list generated

---
+++ app.js
@@ -0,0 +1,5 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "ugly", "beautiful", "last"];
+let noun = ["jogger", "racoon"];
+let footer = [".com", ".net", ".org", ".gov", ".io", ".ui", ".us"];
+
