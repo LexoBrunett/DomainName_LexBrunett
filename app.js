9f41afd896155b5ab1cc2a2ea3db8dcc589eedde
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-08-01 03:22:56 +0000

    function creation

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let adj = ["great", "big", "ugly", "beautiful", "last"];
 let noun = ["jogger", "racoon"];
 let footer = [".com", ".net", ".org", ".gov", ".io", ".ui", ".us"];
 
+let domain;
+function generatedomain() {
+	let pronoum_word = Math.random() * pronoun.length;
+	let adj_word = Math.random() * adj.length;
+	let noun_word = Math.random() * noun.length;
+	let footer_word = Math.random() * footer.length;
+
+	domain= pronoun[pronoum_word]+adj[adj_word]+noun[noun_word]+footer[footer_word];
+
+	return domain;
+}
