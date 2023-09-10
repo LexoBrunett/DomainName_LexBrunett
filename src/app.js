09f163d1aa3e121fca5efb6afdb072068bad5fd8
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-10 15:52:15 +0000

    Changing the app location in src

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "ugly", "beautiful", "last"];
+  let noun = ["jogger", "racoon"];
+  let footer = [".com", ".net", ".org", ".gov", ".io", ".ui", ".us"];
+
+  let domain =
+    adj[getRandom(adj.length)] +
+    "" +
+    noun[getRandom(noun.length)] +
+    "" +
+    footer[getRandom(footer.length)];
+
+  function getRandom(arrayLength) {
+    let number = Math.floor(Math.random() * arrayLength);
+    return number;
+  }
+  console.log(domain);
 };
