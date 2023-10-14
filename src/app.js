d9d9071bd203150c9352b5f340822ec41ec0e47e
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-10-14 16:44:23 +0000

    Good practices corrections

--- src/app.js
+++ src/app.js
@@ -1,32 +1,37 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big", "ugly", "beautiful", "last"];
-  let noun = ["jogger", "racoon"];
-  let footer = [".com", ".net", ".org", ".gov", ".io", ".ui", ".us"];
+  /*Siempre es buena practica diferenciar bien las variables cuando una persona externa lea el codigo */
+  let pronoun_Domain = ["the", "our"];
+  let adj_Domain = ["great", "big", "ugly", "beautiful", "last"];
+  let noun_Domain = ["jogger", "racoon"];
+  let footer_Domain = [".com", ".net", ".org", ".gov", ".io", ".ui", ".us"];
 
   let domain =
-    adj[getRandom(adj.length)] +
+    adj_Domain[getRandom(adj_Domain.length)] +
     "" +
-    noun[getRandom(noun.length)] +
+    noun_Domain[getRandom(noun_Domain.length)] +
     "" +
-    footer[getRandom(footer.length)];
+    footer_Domain[getRandom(footer_Domain.length)];
 
   function getRandom(arrayLength) {
     let number = Math.floor(Math.random() * arrayLength);
     return number;
   }
 
-let domainNames = [];
+  let domainNames = [];
 
-pronoun.forEach((a1) => adj.forEach((a2) => noun.forEach((a3) => domainNames.push(a1 + a2 + a3 + ".com"))));
+  pronoun_Domain.forEach(a1 =>
+    adj_Domain.forEach(a2 =>
+      noun_Domain.forEach(a3 =>
+        footer_Domain.forEach(a4 => domainNames.push(a1 + a2 + a3 + a4))
+      )
+    )
+  );
 
-console.log(domainNames);
-
+  console.log(domainNames);
 };
